75150cd708881cef104897aed816fdbf5c2485d4
griffinhall2 <griffin.hall@yahoo.com>
2018-08-24 07:07:40 -0400

    added routes and friends

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Billy",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGOa1jzKSwtF2rc6QILK4NN9PS9kfDMBDCcAd9BPp-BYj7YiR8",
+		scores: [3, 1, 4, 2, 1, 4, 3, 1, 5, 5]
+	},
+	{
+		name: "Ricky Bobby",
+		photo: "https://www.topcelebsjackets.com/wp-content/uploads/2017/03/Talladega-Nights-Wonder-Bread-Ricky-Bobby-Jacket-6-570x708.jpg",
+		scores: [1, 1, 3, 1, 2, 5, 4, 3, 2, 1]
+	},
+	{
+		name: "Joey",
+		photo: "https://cdn-images-1.medium.com/max/1600/1*fqPCiV7YKyXORMHz172Fig@2x.jpeg",
+		scores: [5, 5, 5, 3, 4, 5, 1, 3, 2, 5]
+	},
+	{
+		name: "Cal",
+		photo: "http://cdn.playbuzz.com/cdn//fe8edea7-7fbb-49d5-85c9-76d3b8ec4930/27b36f37-d8f3-408f-9367-d4c5f2bb5e16.jpg",
+		scores: [1, 4, 3, 1, 2, 3, 2, 2, 2, 2]
+	},
+	{
+		name: "Timmy",
+		photo: ":https://pre00.deviantart.net/39df/th/pre/i/2018/006/9/f/timmy_turner_by_eagc7-dbz4txl.png",
+		scores: [5, 3, 4, 1, 2, 1, 1, 5, 5, 5]
+	}
+];
+
+module.exports = friends;
